f17a837012013c8d4686239b4a2f4c8cac32ed67
cc <chao.hanhanyun@gmail.com>
2023-06-11 14:56:26 +0800

    mmpose第一次作业提交

---
+++ readme.md
@@ -0,0 +1,9 @@
+# mmpose第一期作业
+detection目标检测：MAP:0.77 (200 epoch)
+mmpose人体关键点检测：MAP:0. (280 epoch)
+
+采用方式：shell命令加jupyter完成
+
+关键点检测结果
+
+
---
+++ train.sh
@@ -0,0 +1,5 @@
+# mmdetection
+python /home/hanhan/cc_wrod/develop/mmlab/mmdetection/tools/train.py ./MMPosehomework/configs/rtmdet_tiny_ear.py 
+
+# mmpose
+python tools/train.py ./MMPosehomework/configs/rtmpose_s_ear.py 
